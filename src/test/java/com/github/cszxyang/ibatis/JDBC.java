35d847eb48bb735488c514863ca7c88c76b233ab
cszxyang <linfengit@qq.com>
2020-08-22 12:27:05 +0800

    initial test module

---
+++ src/test/java/com/github/cszxyang/ibatis/JDBC.java
@@ -0,0 +1,8 @@
+package com.github.cszxyang.ibatis;
+
+public interface JDBC {
+  String url = "jdbc:mysql://localhost:3306/test?useSSL=false";
+  String username = "root";
+  String password = "123456";
+  int port = 3306;
+}
---
+++ src/test/java/com/github/cszxyang/ibatis/model/User.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2019-2999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cszxyang.ibatis.model;
+
+import java.io.Serializable;
+
+/**
+ * User 实体类
+ *
+ * @author cszxyang
+ * @since 2020-08-18
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 821465757967990094L;
+    private int id;
+    private String name;
+    private int age;
+
+    public User() {}
+
+    public User(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "id=" + id +
+      ", name='" + name + '\'' +
+      ", age=" + age +
+      '}';
+  }
+}
